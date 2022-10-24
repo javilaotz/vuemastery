e9f97fbcf4d7c5de992c6f01bd72501ef202a7e3
jerikls <jhon.avila@loadsure.net>
2022-10-24 01:31:10 -0500

    feat: conditional rendering using v-if v-else-if v-else

--- tutorial/index.js
+++ tutorial/index.js
@@ -5,6 +5,6 @@ var app = new Vue({
     description: 'Socks world of warcraft horde theme',
     image: 'https://www.impericon.com/media/catalog/product/w/o/worldofwarcraft_horde_accessorie_socks_abysoc005_art20-007512_3665361021285_428969-311_lg.jpg',
     link: 'http://google.com',
-    inStock: false
+    inventory: 100
   }
 })
