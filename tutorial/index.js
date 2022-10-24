8d91e6bdcd3654904d1d64d1ff3e313a1753bb23
jerikls <jhon.avila@loadsure.net>
2022-10-24 02:20:52 -0500

    feat: event handlers, challenge

--- tutorial/index.js
+++ tutorial/index.js
@@ -1,9 +1,9 @@
 var app = new Vue({
   el: "#app",
   data: {
-    product: 'Horde Socks',
+    product: 'Vue Socks',
     description: 'Socks world of warcraft horde theme',
-    image: 'https://www.impericon.com/media/catalog/product/w/o/worldofwarcraft_horde_accessorie_socks_abysoc005_art20-007512_3665361021285_428969-311_lg.jpg',
+    image: './images/green_socks.png',
     link: 'http://google.com',
     inventory: 100,
     onSale: true,
@@ -12,18 +12,26 @@ var app = new Vue({
       {
         variantId: 2234,
         variantColor: "green",
+        variantImage: "./images/green_socks.png"
       },
       {
         variantId: 2235,
         variantColor: "blue",
+        variantImage: "./images/blue_socks.png"
       }
     ],
     sizes: ["xs", "s", "m", "l", "xl", "xxl", "xxxl"],
     cart: 0
   },
   methods: {
-    addToCart() {
+    addToCart: function () {
       this.cart += 1;
+    },
+    removeFromCart: function () {
+      this.cart -= 1;
+    },
+    updateProduct: function (image) {
+      this.image = image;
     }
   }
 })
