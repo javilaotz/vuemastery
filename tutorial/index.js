dbaa9bdb7f3b19ce613ba8fb8447190576cf2a9e
jerikls <jhon.avila@loadsure.net>
2022-10-24 00:50:09 -0500

    totorial files

---
+++ tutorial/index.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: 'Horde Socks',
+    description: 'Socks world of warcraft horde theme',
+    image: 'https://www.impericon.com/media/catalog/product/w/o/worldofwarcraft_horde_accessorie_socks_abysoc005_art20-007512_3665361021285_428969-311_lg.jpg'
+  }
+})
