8664c992324b96f0043fd0312f52360961f1b310
jerikls <jhon.avila@loadsure.net>
2022-10-24 03:15:56 -0500

    feat: computed props

--- tutorial/index.js
+++ tutorial/index.js
@@ -4,22 +4,23 @@ var app = new Vue({
     brand: 'vueMastery',
     product: 'Vue Socks',
     description: 'Socks world of warcraft horde theme',
-    image: './images/green_socks.png',
     link: 'http://google.com',
-    inventory: 20,
-    inStock: false,
-    onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    selectedVariant: 0,
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "./images/green_socks.png"
+        variantImage: "./images/green_socks.png",
+        variantInventory: 20,
+        onSale: true
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "./images/blue_socks.png"
+        variantImage: "./images/blue_socks.png",
+        variantInventory: 0,
+        onSale: false
       }
     ],
     sizes: ["xs", "s", "m", "l", "xl", "xxl", "xxxl"],
@@ -43,13 +44,25 @@ var app = new Vue({
       this.cart -= 1;
       this.inventory += 1;
     },
-    updateProduct: function (image) {
-      this.image = image;
+    updateProduct: function (index) {
+      this.selectedVariant = index;
     }
   },
   computed: {
     title(){
       return `${this.brand} | ${this.product} ${this.onSale && 'On SALE!'}`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantInventory > 0
+    },
+    inventory() {
+      return this.variants[this.selectedVariant].variantInventory
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].variantInventory
     }
   }
 })
