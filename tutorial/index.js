d822544baf38cc31bb694657349841b623205c66
jerikls <jhon.avila@loadsure.net>
2022-10-24 01:56:03 -0500

    feat: v-for

--- tutorial/index.js
+++ tutorial/index.js
@@ -6,6 +6,18 @@ var app = new Vue({
     image: 'https://www.impericon.com/media/catalog/product/w/o/worldofwarcraft_horde_accessorie_socks_abysoc005_art20-007512_3665361021285_428969-311_lg.jpg',
     link: 'http://google.com',
     inventory: 100,
-    onSale: true
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+      }
+    ],
+    sizes: ["xs", "s", "m", "l", "xl", "xxl", "xxxl"]
   }
 })
