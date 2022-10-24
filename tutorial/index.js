7f9300b1b4b7dcc1fed621efad3eaade328181e9
jerikls <jhon.avila@loadsure.net>
2022-10-24 01:25:30 -0500

    feat: challenge

--- tutorial/index.js
+++ tutorial/index.js
@@ -3,6 +3,7 @@ var app = new Vue({
   data: {
     product: 'Horde Socks',
     description: 'Socks world of warcraft horde theme',
-    image: 'https://www.impericon.com/media/catalog/product/w/o/worldofwarcraft_horde_accessorie_socks_abysoc005_art20-007512_3665361021285_428969-311_lg.jpg'
+    image: 'https://www.impericon.com/media/catalog/product/w/o/worldofwarcraft_horde_accessorie_socks_abysoc005_art20-007512_3665361021285_428969-311_lg.jpg',
+    link: 'http://google.com'
   }
 })
