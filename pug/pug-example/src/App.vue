454624d5d38f44bd1e5be9c26d0f109b70afc79e
jerikls <jhon.avila@loadsure.net>
2022-11-17 09:17:48 -0500

    feat: adding pug example with vuejs installer

---
+++ pug/pug-example/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template lang="pug">
+div
+  header
+    img.logo(alt='Vue logo' src='./assets/logo.svg' width='125' height='125')
+    .wrapper
+      HelloWorld(msg='You did it!')
+  main
+    TheWelcome
+</template>
+
+<style scoped>
+div header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  div header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  div header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
